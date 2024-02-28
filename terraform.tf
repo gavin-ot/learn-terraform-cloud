7a4771c35ccfa6860b9bdbab15ad44c530b768b6
gavin <gavin@optional-stack.com>
2024-02-28 21:07:23 +0800

    fist commit

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "only-terraform"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
